007d3774cac7c270c607f1d6c86e0ca120589809
Raga2629 <nasaniragamala@gmail.com>
2026-03-10 10:26:42 +0530

    Update axios.js

--- trustbridge-v2/src/api/axios.js
+++ trustbridge-v2/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://trustbridge-backend-i48b.onrender.com',
+  baseURL: 'https://trustbridge-backend-i48b.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
